4119847d50844f59fe0089109a08563d30866a86
Maivannan <maivannan1997@gmail.com>
2025-01-01 22:26:48 +0530

    changes in repositories

--- User service/User-service/src/main/java/com/onlineshopping/userService/repository/UserRepository.java
+++ User service/User-service/src/main/java/com/onlineshopping/userService/repository/UserRepository.java
@@ -1,13 +1,12 @@
 package com.onlineshopping.userService.repository;
 
-import com.onlineshopping.userService.entities.ERole;
-import com.onlineshopping.userService.entities.Role;
 import com.onlineshopping.userService.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-
+@Repository
 public interface UserRepository extends JpaRepository<User,Long> {
 
 
